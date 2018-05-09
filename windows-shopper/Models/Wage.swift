a8a9a84e772403859c72cc639eed002682e8369b
thanhbh <thanhbh@admins-imac.local>
2018-05-09 10:08:07 +0700

    add model

---
+++ windows-shopper/Models/Wage.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  windows-shopper
+//
+//  Created by thanhbh on 5/9/18.
+//  Copyright © 2018 thanhbh. All rights reserved.
+//
+
+import Foundation
+
+class Wage {
+    class func getHours(forWage wage: Double, andPrice price: Double) -> Int {
+        return Int(cei(price / wage))
+    }
+}
