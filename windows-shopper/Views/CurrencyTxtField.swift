22ee70e13ab9a20401e746036a565e1f277d5d98
thanhbh <thanhbh@admins-imac.local>
2018-05-09 11:55:55 +0700

    update currency text field

--- windows-shopper/Views/CurrencyTxtField.swift
+++ windows-shopper/Views/CurrencyTxtField.swift
@@ -10,29 +10,40 @@ import UIKit
 
 @IBDesignable
 class CurrencyTxtField: UITextField {
-
-    /*
+    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         // Drawing code
+        let size: CGFloat = 20
+        let lblCurrency = UILabel(frame: CGRect(x: 5, y: (frame.size.height - size) / 2, width: size, height: size))
+        lblCurrency.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.7992957746)
+        lblCurrency.textAlignment = .center
+        lblCurrency.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        lblCurrency.layer.cornerRadius = 5.0
+        lblCurrency.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        lblCurrency.text = formatter.currencySymbol
+        addSubview(lblCurrency)
     }
-    */
     
     fileprivate func customizeView() {
-        //        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
-        backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.25)
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        //backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.25)
         borderStyle = .none
-        layer.cornerRadius = 10.0
+        layer.cornerRadius = 5.0
         textAlignment = .center
         font = UIFont(name: "Avenir-Medium", size: 15)
         keyboardType = .decimalPad
         enablesReturnKeyAutomatically = true
+        clipsToBounds = true
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
             attributedPlaceholder = place
-            //        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-            textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            //textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
         }
     }
     
@@ -44,5 +55,5 @@ class CurrencyTxtField: UITextField {
         super.awakeFromNib()
         customizeView()
     }
-
+    
 }
