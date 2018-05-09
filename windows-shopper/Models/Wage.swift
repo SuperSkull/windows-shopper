2becedf81bb08e2f903ebb5f98bc387b054c8f64
thanhbh <thanhbh@admins-imac.local>
2018-05-09 11:11:44 +0700

    update wage model

--- windows-shopper/Models/Wage.swift
+++ windows-shopper/Models/Wage.swift
@@ -10,6 +10,6 @@ import Foundation
 
 class Wage {
     class func getHours(forWage wage: Double, andPrice price: Double) -> Int {
-        return Int(cei(price / wage))
+        return Int(ceil(price / wage))
     }
 }
