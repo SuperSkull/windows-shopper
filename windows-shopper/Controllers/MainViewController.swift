d36c30dac7a31107bed26d9a07c1061174534bf9
thanhbh <thanhbh@admins-imac.local>
2018-05-10 11:46:48 +0700

    update ui

--- windows-shopper/Controllers/MainViewController.swift
+++ windows-shopper/Controllers/MainViewController.swift
@@ -21,11 +21,11 @@ class MainViewController: UIViewController {
         self.view.addGestureRecognizer(tapGestureBackground)
         // Do any additional setup after loading the view, typically from a nib.
         let btnCalculator = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
-//        btnCalculator.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
-        btnCalculator.backgroundColor = UIColor(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        btnCalculator.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+//        btnCalculator.backgroundColor = UIColor(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
         btnCalculator.setTitle("Calculator", for: .normal)
-//        btnCalculator.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
-        btnCalculator.setTitleColor(UIColor(white: 1, alpha: 1), for: .normal)
+        btnCalculator.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+//        btnCalculator.setTitleColor(UIColor(white: 1, alpha: 1), for: .normal)
         btnCalculator.addTarget(self, action: #selector(MainViewController.calculate), for: .touchUpInside)
         txtWage.inputAccessoryView = btnCalculator
         txtPrice.inputAccessoryView = btnCalculator
